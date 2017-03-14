5ce2c645c7385e351b6eba89034ad57f952f8ecc
Nanni Bassetti <nannib@users.noreply.github.com>
2017-03-14 11:35:43 +0100

    Add files via upload

--- nbtempo1.pas
+++ nbtempo1.pas
@@ -17,6 +17,8 @@ type
     Button2: TButton;
     Button3: TButton;
     Button4: TButton;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
     DateTimePicker1: TDateTimePicker;
     DateTimePicker2: TDateTimePicker;
     DirectoryEdit1: TDirectoryEdit;
@@ -28,27 +30,28 @@ type
     Label5: TLabel;
     Label6: TLabel;
     Label7: TLabel;
-    LabeledEdit1: TLabeledEdit;
     LabeledEdit2: TLabeledEdit;
     ProgressBar1: TProgressBar;
     Splitter1: TSplitter;
     Splitter2: TSplitter;
     StaticText1: TStaticText;
-    StaticText2: TStaticText;
     StaticText3: TStaticText;
     StaticText4: TStaticText;
     StaticText5: TStaticText;
+    StaticText6: TStaticText;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
     procedure Button4Click(Sender: TObject);
+    procedure CheckBox1Change(Sender: TObject);
+    procedure CheckBox2Change(Sender: TObject);
     procedure DirectoryEdit1Change(Sender: TObject);
     procedure Edit3Change(Sender: TObject);
     procedure FileNameEdit1Change(Sender: TObject);
     procedure FormStartDock(Sender: TObject; var DragObject: TDragDockObject);
-    procedure LabeledEdit1Change(Sender: TObject);
     procedure ProgressBar1ContextPopup(Sender: TObject; MousePos: TPoint;
       var Handled: Boolean);
+    procedure StaticText3Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -58,7 +61,7 @@ type
 var
   Form1: TForm1;
   selectedfile: string;
-  z,d1,d2,d,macd,dcsv,ctime,fn,dn: string;
+  z,d1,d2,d,macd,dcsv,ctime,fn,dn,zulu,zl,skew: string;
   ActualTime: TDateTime;
 
 
@@ -73,19 +76,22 @@ implementation
 procedure TForm1.FormStartDock(Sender: TObject; var DragObject: TDragDockObject
   );
 begin
-
+  zulu:='';
+  zl:='';
 end;
 
-procedure TForm1.LabeledEdit1Change(Sender: TObject);
-begin
-
-end;
 
 procedure TForm1.ProgressBar1ContextPopup(Sender: TObject; MousePos: TPoint;
   var Handled: Boolean);
 begin
 
 end;
+
+procedure TForm1.StaticText3Click(Sender: TObject);
+begin
+
+end;
+
 procedure ExecuteAndWait(const aCommando: string);
 var
   tmpStartupInfo: TStartupInfo;
@@ -133,7 +139,7 @@ begin
      if (DateTimePicker1.DateIsNull) and (DateTimePicker2.DateIsNull) then
      begin
        d:='';
-       macd:='-d 0000-00-00';
+       macd:=zulu+' -d 0000-00-00';
        dcsv:='All';
      end
      else
@@ -141,24 +147,22 @@ begin
        d1:=FormatDateTime('yyyy-mm-dd', DateTimePicker1.Date);
        d2:=FormatDateTime('yyyy-mm-dd', DateTimePicker2.Date);
        d:=d1+'_'+d2;
-       macd:='-d '+d1+'..'+d2;
+       macd:=zulu+' -d '+d1+'..'+d2;
        dcsv:=d;
      end;
-     if  (LabeledEdit1.Text='') then
+     if (LabeledEdit2.Text='') then
      begin
-       z:=' -s '+LabeledEdit2.Text+' ';
-     end
-     else
-     begin
-       z:=' -z '+LabeledEdit1.Text+' -s '+LabeledEdit2.Text+' ';
+       LabeledEdit2.Text:='0';
      end;
-     //ProgressBar1.StepIt;
-       ExecuteAndWait('cmd /c .\bin\tsk_gettimes.exe '+z+fn+'| .\bin\mactime.exe '+macd+' >'+dn+'\timeline'+dcsv+'_C'+ctime+'.csv');
 
+     z:=' -s '+LabeledEdit2.Text+' ';
+     skew:='skew'+LabeledEdit2.Text;
+     //ProgressBar1.StepIt;
+       ExecuteAndWait('cmd /c .\bin\tsk_gettimes.exe '+z+fn+'| .\bin\mactime.exe '+macd+' >'+dn+'\timeline'+dcsv+'_C'+ctime+zl+skew+'.csv');
 
      Progressbar1.Visible:=False;
      Label2.Visible:=False;
-     Edit3.Text:=dn+'\timeline'+dcsv+'_C'+ctime+'.csv';
+     Edit3.Text:=dn+'\timeline'+dcsv+'_C'+ctime+zl+skew+'.csv';
  end;
 
 procedure TForm1.Button2Click(Sender: TObject);
@@ -168,13 +172,35 @@ end;
 
 procedure TForm1.Button3Click(Sender: TObject);
 begin
-  ShellExecute(handle,'open',PChar(dn+'\timeline'+dcsv+'_C'+ctime+'.csv'),'','',1)
+  ShellExecute(handle,'open',PChar(dn+'\timeline'+dcsv+'_C'+ctime+zl+skew+'.csv'),'','',1)
 end;
 
 procedure TForm1.Button4Click(Sender: TObject);
 begin
   DateTimePicker1.Date:=1.7E308;
   DateTimePicker2.Date:=1.7E308;
+end;
+
+
+procedure TForm1.CheckBox1Change(Sender: TObject);
+begin
+  if (CheckBox1.Checked) then
+  begin
+    CheckBox2.Checked:=FALSE;
+    CheckBox1.Checked:=TRUE;
+   zulu:='-y';
+   zl:='ZULU';
+   end
+   else
+   begin
+   zulu:='';
+   zl:='';
+   end;
+end;
+
+procedure TForm1.CheckBox2Change(Sender: TObject);
+begin
+  CheckBox1.Checked:=FALSE;
 end;
 
 procedure TForm1.DirectoryEdit1Change(Sender: TObject);
